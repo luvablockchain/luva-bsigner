d71182c1fa7eeea99dc8fd880041c466f23f65bf
sonh <son@huynh.dev>
2019-09-19 11:54:49 +0700

    Backup

--- app/src/main/java/com/luvapay/bsigner/MainActivity.kt
+++ app/src/main/java/com/luvapay/bsigner/MainActivity.kt
@@ -70,7 +70,7 @@ class MainActivity : BaseActivity() {
             }
         }
 
-        btnAuthNew.setOnClickListener {
+        home_create.setOnClickListener {
             startActivity<BackupActivity>()
         }
 
--- app/src/main/java/com/luvapay/bsigner/activities/CreateMnemonicsActivity.kt
+++ app/src/main/java/com/luvapay/bsigner/activities/CreateMnemonicsActivity.kt
@@ -10,6 +10,7 @@ import com.luvapay.bsigner.items.MenemonicItem
 import com.luvapay.bsigner.R
 import com.luvapay.bsigner.createBip39Seed
 import com.mikepenz.fastadapter.adapters.FastItemAdapter
+import com.soneso.stellarmnemonics.Wallet
 import kotlinx.android.synthetic.main.activity_create_mnemonics.*
 
 class CreateMnemonicsActivity : AppCompatActivity() {
@@ -18,8 +19,8 @@ class CreateMnemonicsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_mnemonics)
 
-        val mnemonics = "bench hurt jump file august wise shallow faculty impulse spring exact slush thunder author capable act festival slice deposit sauce coconut afford frown better"
-        //val mnemonics = Wallet.generate12WordMnemonic().toString()
+        //val mnemonics = "bench hurt jump file august wise shallow faculty impulse spring exact slush thunder author capable act festival slice deposit sauce coconut afford frown better"
+        val mnemonics = String(Wallet.generate12WordMnemonic())
 
         val bip39Seed = createBip39Seed(mnemonics.toCharArray())
 
